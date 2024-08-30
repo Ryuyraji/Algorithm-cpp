f619af36d6db076a392fe51c3fa855013edb7083
Ryuyraji <yeaji1839@gmail.com>
2024-08-30 11:44:54 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -2,10 +2,9 @@
 using namespace std;
 int main() {
     int A, B, V;
-    int x, y;
     int day = 0;
     cin >> A >> B >> V;
-    //(A - B)* (day - 1) + A <= V >= (A - B) * (day - 1) + 1;
+    //(A - B)* (day - 1) + A <= V 
     if ((V - A) % (A - B) == 0) {
         day = (V - A) / (A - B) + 1;
     }
