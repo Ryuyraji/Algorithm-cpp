53ba2617dae1536fa0ff4f82404c37051ea52265
Ryuyraji <yeaji1839@gmail.com>
2024-09-03 19:34:13 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    string S;
+    int i;
+    cin >> S;
+    for (i = 0; i < S.length(); i++) {
+       
+   }
+    cout << i;
+    return 0;
+}
