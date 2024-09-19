0d41eb0e1c5002a77b7ceec8c7bf547999840e96
Ryuyraji <yeaji1839@gmail.com>
2024-09-19 22:35:11 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;  
+    cout << "|\\_/|" << '\n';        
+    cout << "|q p|   /}" << '\n';   
+    cout << "( 0 )\"\"\"\\" << '\n'; 
+    cout << "|\"^\"`    |" << '\n';  
+    cout << "||_/=\\\\__|" << '\n';   
+}
+
