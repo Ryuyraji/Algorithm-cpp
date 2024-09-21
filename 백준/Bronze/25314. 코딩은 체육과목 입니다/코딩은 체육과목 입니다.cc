61858f29f7ecb7571a6c5d87c42cf0b9fc9494ba
Ryuyraji <yeaji1839@gmail.com>
2024-09-21 13:25:01 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    int N;
+    cin >> N;
+    for (i = 0; i < N/4; i++) {
+        cout << "long"<<' ';
+    }
+    cout << "int";
+}
