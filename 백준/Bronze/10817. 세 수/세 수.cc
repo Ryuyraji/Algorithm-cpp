bf29bdf9804600bf62ce971308b1f8f2b08801c2
Ryuyraji <yeaji1839@gmail.com>
2024-12-20 23:00:43 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int second;
+
+    if ((a >= b && a <= c) || (a <= b && a >= c)) {
+        second = a;
+    }
+    else if ((b >= a && b <= c) || (b <= a && b >= c)) {
+        second = b;
+    }
+    else {
+        second = c;
+    }
+
+    cout << second << endl;
+    return 0;
+}
